8077c0573c8a4f030c890f0fd626f4b7d2692de7
Wiliam Meurer <50109787+WILIAMJOAQUIMMEURER@users.noreply.github.com>
2022-05-09 19:48:57 -0300

    adicionado transfor metros em cm

---
+++ calculo_media_notas.py
@@ -0,0 +1,11 @@
+print('********* Bem Vindo *********' )
+print('***Projeto Iniciando Python***')
+
+nota1 = int (input("Digite a Primeira nota !"))
+
+nota2 = int (input("Digite a Segunda  nota !"))
+
+media = (nota1+nota2) /2
+
+print("As suas notas foram {} e {} e a media delas é {}".format(nota1,nota2,media))
+
---
+++ transformar_metros_em_centimetros.py
@@ -0,0 +1,8 @@
+print('********* Bem Vindo *********' )
+print('***Projeto Iniciando Python***')
+
+metros = float (input("Digite o Valor em Metros ! "))
+
+conversao = metros * 100
+
+print("o valor a ser convertido  é {} metros = {} cm ".format(metros,conversao))
