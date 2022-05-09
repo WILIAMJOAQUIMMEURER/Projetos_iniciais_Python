17e3dffe4845e07a2222b5801cd9b5d1919d80ac
Wiliam Meurer <50109787+WILIAMJOAQUIMMEURER@users.noreply.github.com>
2022-05-09 20:30:51 -0300

    adicionado tabuada

---
+++ fazendo_tabuadas.py
@@ -0,0 +1,11 @@
+print('********* Bem Vindo *********' )
+print('***Projeto Iniciando Python***')
+
+numero = int (input("Escolha um numero para fazer a tabuada !"))
+
+incremento = 1
+
+
+while incremento <= 10:
+    print(numero, 'x', incremento, '=', numero*incremento)
+    incremento = incremento + 1
